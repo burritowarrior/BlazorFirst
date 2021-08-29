85284ea42ccf5190cbcdb0372fcaef0d692bf245
Christopher Oson <coson16@gmail.com>
2021-08-29 10:49:25 -0700

    Reorganizing Code

---
+++ Models/Country.cs
@@ -0,0 +1,8 @@
+namespace Models
+{
+    public class Country
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/StatesMeta.cs
@@ -0,0 +1,13 @@
+namespace Models
+{
+    public class StatesMeta
+    {
+        public string StateName { get; set; }
+        public string Abbreviation { get; set; }
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public int Area { get; set; }
+        public System.DateTime DateEntered { get; set; }
+        public int DateRank { get; set; }
+    }
+}
